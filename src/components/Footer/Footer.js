80a7463613c4d416f1b3fcb93311c625d2cef771
NadiaMahbuba <mahbubamomo638@gmail.com>
2022-04-29 17:09:59 +0600

    responsive part added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className='footer'>
-            <h3>Differentiate between props and state?</h3>
+            <h2>Differentiate between props and state?</h2>
             <p>There are many differences exist between props and state they are-
                 1)props are immutable on the other hand states are mutable
                 that means props can not be modified and states are able to modified.
@@ -13,8 +13,8 @@ const Footer = () => {
                 4)State and other components can used props,but state only used within state component.
                 5)Props are used for communicating within components,Dynamic changes within components done by state.
             </p>
-            <h3>How use state work?</h3>
-            <p>When we declare a state variable with useState , it returns a pair — an array with two items. The first item is the current value, and the second is a function.useState is a Hook (function) that allows you to have state variables in functional components. You pass the initial state to this function and it returns a variable with the current state value (not necessarily the initial state) and another function to update this value.</p>
+            <h2>How use state work?</h2>
+            <p>When we declare a state variable using useState , it returns a pair — an array with two items. The first item is the current value, and the second is a function.useState is a Hook (function) that is used for  declaring state variables in functional components. We first set the initial state of this function then it returns a variable with the current state value .And the return function is used to update the value of the variable.That's the  working process of useState.</p>
         </div>
     );
 };
